a9dff9414121d13b9794c6bb3416ddae5cc3145c
Haradorn <44273673+Haradorn@users.noreply.github.com>
2021-04-04 22:22:36 +0300

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersLoginPassword.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        [MinLength(7)]
+        public string Password { get; set; }
+    }
+}
